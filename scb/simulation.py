def530d7b43b6e4c3712344be448c71ef2392b03
Aurelio <Aurelio.ortale@gmx.de>
2021-04-27 17:43:41 +0200

    better class definition

--- scb/simulation.py
+++ scb/simulation.py
@@ -5,22 +5,27 @@ from win32api import GetSystemMetrics
 # Initalization of a Creation Object as Class with different properties
 
 class Creature:
-    def __init__(self, name, x_position, y_position, properties = 0, stomach = 0): # Stomach and properties will be added later - name is just for fun
-        self.name = name
-        self.x_position = x_position
-        self.y_position = y_position
+    def __init__(self, name, properties = 0, stomach = 0): # Stomach and properties will be added later - name is just for fun   
+        self.name = name  
         self.properties = properties
         self.stomach = stomach
-        
-    def CreationOfCreatur(self):
-        y = int(GetSystemMetrics(0)) # Detecting the width and hight of the monitor for drawing a window
-        x = int(GetSystemMetrics(1))
 
-        win = GraphWin(self.name + "'s crazy life", y, x) # Draws the window with Creaturs name and scaling of monitor
-        c = Circle(Point(self.x_position,self.y_position),30) # Circle as representation of the creature - for now
-        c.draw(win)
-        win.getMouse() # Waiting command for the window until a click appears
-        win.close
+        self.MakeWindow()
+        self.CreationOfCreatur(self.name)
 
-x = Creature(input("Please enter a name for the Creature:  "),input("Please enter a X-Position for your Creature: "),input("Please enter a Y-Position for your Creature: "))
-x.CreationOfCreatur()
+    def MakeWindow(self):
+        self.hight = GetSystemMetrics(1) # Detecting the width and hight of the monitor for drawing a window
+        self.wide = GetSystemMetrics(0)
+        self.window = (self.wide, self.hight)
+        return self.window
+                
+    def CreationOfCreatur(self, name):
+        self.name = str(name)
+        self.win = GraphWin(self.name + "'s crazy life", self.MakeWindow()[0], self.MakeWindow()[1]) # Draws the window with Creaturs name and scaling of monitor
+        c = Circle(Point(random.randrange(0, self.MakeWindow()[0],1),random.randrange(0, self.MakeWindow()[1],1)),30) # Circle as representation of the creature - for now
+        c.setFill("red")
+        c.draw(self.win)
+        self.win.getMouse() # Waiting command for the window until a click appears
+        self.win.close
+
+x = Creature("Bob")
