d42edba1e5ba681b1cd3dcf838147a1ff877c26f
Aurelio <Aurelio.ortale@gmx.de>
2021-05-01 12:37:01 +0200

    Try to applied borders and colors

--- scb/simulation.py
+++ scb/simulation.py
@@ -1,5 +1,6 @@
 import random
 import time
+
 from graphics import *
 from win32api import GetSystemMetrics
 
@@ -10,8 +11,10 @@ class Creatures:
     creature_count = 0
     list_c = [] 
     list_names = [] 
+    list_positions_of_creatures = []
     hight = GetSystemMetrics(1) 
     wide = GetSystemMetrics(0)
+    my_color = ("red","yellow","blue","green","purple")
 
     # Initial properties of a crature
     def __init__(self, properties = 0, stomach = 0): # Stomach and properties will be added later - name is just for fun   
@@ -26,14 +29,15 @@ class Creatures:
     # The Function for creating a Creature, with name, color, a size and adjusted window sizes so cratures spawn not outside             
     def CreationOfCreature(self, name = "Bob"):
         self.name = name 
-        self.color = ("red","green","blue","pink") # Example for color pallet -> use a real one
         self.circle_diameter = 30 
         self.adjusted_hight = self.hight - self.circle_diameter
         self.adjusted_wide = self.wide - self.circle_diameter
 
         self.list_names.append(self.name) 
-        c = Circle(Point(random.randrange(0, self.adjusted_wide, 1),random.randrange(0, self.adjusted_hight, 1)), self.circle_diameter) # Circle as representation of the creature - for now
-        c.setFill(self.color[self.creature_count]) # Different colors for the different creatures based on their creation
+        self.position_creature = (random.randrange(0, self.adjusted_wide, 1),random.randrange(0, self.adjusted_hight, 1))
+        self.list_positions_of_creatures.append(self.position_creature)
+        c = Circle(Point(self.position_creature[0],self.position_creature[1]), self.circle_diameter) # Circle as representation of the creature - for now
+        c.setFill(self.my_color[self.creature_count]) # Different colors for the different creatures based on their creation
         self.list_c.append(c) 
         self.creature_count += 1 
 
@@ -42,26 +46,36 @@ class Creatures:
         for i in range(len(self.list_c)): # Draws all cratures from the list to the crated window
           self.list_c[i].draw(self.win)
 
-        # Moves aroud all creatures randomly until the interruption of the consols happend
+        # Moves aroud all creatures randomly (until hitting the border) until the interruption of the consols happend 
+        x = 0
         try:
-            while True:
-                for i in range(len(self.list_c)):
-                    self.list_c[i].move(random.randrange(-10,10,1),random.randrange(-10,10,1))
-                    time.sleep(0.005)
+            while (True):
+                for i in range(len(self.list_c)): # all creatures of the list
+                    if (self.list_positions_of_creatures[i][0] >= self.wide - 40) or (self.list_positions_of_creatures[i][1] >= self.hight - 40): # better way for border detection !!
+                        self.list_c[i].move(random.randrange(-10,0,1),random.randrange(-10,0,1))
+                        time.sleep(0.01)
+
+                    elif(self.list_positions_of_creatures[i][0] <= 0 + 40) or (self.list_positions_of_creatures[i][1] <= 0 + 40):
+                        self.list_c[i].move(random.randrange(0,10,1),random.randrange(0,10,1))
+                        time.sleep(0.01)
+
+                    else:
+                        self.list_c[i].move(random.randrange(-10,10,1),random.randrange(-10,10,1))
+                        time.sleep(0.01)
 
         except KeyboardInterrupt: # Better solution for Movement stop and canceling !
             self.win.getMouse()
             self.win.close 
-              
+
 #Test Area where I crate three cratures in the variable x and crate a Window as well as drawing the cratures to it
 x = Creatures()
 
 while True:
     if(x.creature_count == 0):
         x.CreationOfCreature(input("Please choose a name for your first Creature:  "))
-    if(x.creature_count > 0) and (x.creature_count <= 4):
+    if(x.creature_count > 0) and (x.creature_count <= 5):
         while True:
-            z = input("Do you want to create another creature(max = 4) [y/n]")
+            z = input("Do you want to create another creature(max = 5) [y/n]")
             z = str(z)
             if (z == "y"):
                 x.CreationOfCreature(input("Please choose a name for another Creature:  "))
@@ -71,7 +85,7 @@ while True:
                 x.DrawCreatures()
                 break
             print("Your answer was incorrect")
-    if(x.creature_count >= 4):
-        print("Sry maximal creature number is 4")
+    if(x.creature_count >= 5):
+        print("Sry maximal creature number is 5")
         x.CreationOfWindow()
         x.DrawCreatures()
